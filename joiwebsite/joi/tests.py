28518bc2aafcfb5e5b12dbe2badf8fbafa81d0c0
Ty Seddon <ty.seddon@cognivista.com>
2022-01-29 11:25:32 -0500

    refactor tests

--- joiwebsite/joi/tests.py
+++ joiwebsite/joi/tests.py
@@ -4,6 +4,22 @@ import joi.data as data
 from munch import munchify
 import json
 
+RESIDENTS_URL = '/joi/v1/residents/'
+
+def create_client(username, password):
+    c = Client()
+    if username is not None:
+        login_response = c.login(username=username, password=password)
+        if not login_response:
+            raise Exception('Failed to login %s' % (username))
+    return c
+
+def response_to_list(response):
+    return [munchify(o) for o in json.loads(response.content)]
+
+def response_to_object(response):
+    return munchify(json.loads(response.content))
+
 class HomeTestCase(TestCase):
     def setUp(self):
         data.initialize_data()
@@ -13,38 +29,69 @@ class HomeTestCase(TestCase):
         response = c.get("/")
         self.assertEqual(200, response.status_code)
 
+class LoginTestCase(TestCase):
+    def setUp(self):
+        data.initialize_data()
+
+    def test_carepartner_1(self):
+        c = Client()
+        login_response = c.login(username='cp_1', password='testpassword')
+        self.assertEqual(True, login_response)
+
+    def test_carepartner_2(self):
+        c = Client()
+        login_response = c.login(username='cp_2', password='testpassword')
+        self.assertEqual(True, login_response)
 
 class ResidentTestCase(TestCase):
     def setUp(self):
         data.initialize_data()
 
     def test_list_resident_by_anonymous(self):
-        c = Client()
-        response = c.get("/joi/v1/residents/")
+        c = create_client(None,None)
+        response = c.get(RESIDENTS_URL)
         self.assertEqual(401, response.status_code)
 
+    ### Care Partner 1 ########################
+
     def test_list_resident_by_carepartner_1(self):
-        c = Client()
-        login_response = c.login(username='cp_1', password='testpassword')
-        self.assertEqual(True, login_response)
-        response = c.get("/joi/v1/residents/")
+        c = create_client('cp_1','testpassword')
+        response = c.get(RESIDENTS_URL)
         self.assertEqual(200, response.status_code)
-        objs = [munchify(o) for o in json.loads(response.content)]
+        objs = response_to_list(response)
         self.assertEqual(1, len(objs))
         self.assertEqual(str(data.UUID_1), objs[0].resident_id)
 
     def test_get_resident_1_by_carepartner_1(self):
-        c = Client()
-        login_response = c.login(username='cp_1', password='testpassword')
-        self.assertEqual(True, login_response)
-        response = c.get("/joi/v1/residents/00000000-0000-0000-0000-000000000001/")
+        c = create_client('cp_1','testpassword')
+        response = c.get('%s%s/' % (RESIDENTS_URL,data.UUID_1))
         self.assertEqual(200, response.status_code)
-        obj = munchify(json.loads(response.content))
+        obj = response_to_object(response)
         self.assertEqual(str(data.UUID_1), obj.resident_id)
 
     def test_get_resident_2_by_carepartner_1(self):
-        c = Client()
-        login_response = c.login(username='cp_1', password='testpassword')
-        self.assertEqual(True, login_response)
-        response = c.get("/joi/v1/residents/00000000-0000-0000-0000-000000000002/")
+        c = create_client('cp_1','testpassword')
+        response = c.get('%s%s/' % (RESIDENTS_URL,data.UUID_2))
+        self.assertEqual(403, response.status_code)
+
+    ### Care Partner 2 ########################
+
+    def test_list_resident_by_carepartner_2(self):
+        c = create_client('cp_2','testpassword')
+        response = c.get(RESIDENTS_URL)
+        self.assertEqual(200, response.status_code)
+        objs = response_to_list(response)
+        self.assertEqual(1, len(objs))
+        self.assertEqual(str(data.UUID_2), objs[0].resident_id)
+
+    def test_get_resident_2_by_carepartner_2(self):
+        c = create_client('cp_2','testpassword')
+        response = c.get('%s%s/' % (RESIDENTS_URL,data.UUID_2))
+        self.assertEqual(200, response.status_code)
+        obj = response_to_object(response)
+        self.assertEqual(str(data.UUID_2), obj.resident_id)
+
+    def test_get_resident_1_by_carepartner_2(self):
+        c = create_client('cp_2','testpassword')
+        response = c.get('%s%s/' % (RESIDENTS_URL,data.UUID_1))
         self.assertEqual(403, response.status_code)
